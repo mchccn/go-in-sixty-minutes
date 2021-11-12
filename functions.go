2a1b59c3dab6bea0506f56f3680e80a7fcdd6437
[Cursors] <65373746+cursorsdottsx@users.noreply.github.com>
2021-11-12 13:38:01 -0600

    Create functions.go

---
+++ functions.go
@@ -0,0 +1,21 @@
+/*
+A function can take zero or more arguments.
+
+In this example, add takes two parameters of type int.
+
+Notice that the type comes after the variable name.
+
+(For more about why types look the way they do, see the article on Go's declaration syntax.)
+*/
+
+package main
+
+import "fmt"
+
+func add(x int, y int) int {
+	return x + y
+}
+
+func main() {
+	fmt.Println(add(42, 13))
+}
