109d9cb72675ea0914ec2966db35f31867e15f07
[Cursors] <65373746+cursorsdottsx@users.noreply.github.com>
2021-11-12 13:33:35 -0600

    Create hello.go

---
+++ hello.go
@@ -0,0 +1,32 @@
+/*
+Hello, 世界
+Welcome to a tour of the Go programming language.
+
+The tour is divided into a list of modules that you can access by clicking on A Tour of Go on the top left of the page.
+
+You can also view the table of contents at any time by clicking on the menu on the top right of the page.
+
+Throughout the tour you will find a series of slides and exercises for you to complete.
+
+You can navigate through them using
+
+"previous" or PageUp to go to the previous page,
+"next" or PageDown to go to the next page.
+The tour is interactive. Click the Run button now (or press Shift + Enter) to compile and run the program on a remote server. The result is displayed below the code.
+
+These example programs demonstrate different aspects of Go. The programs in the tour are meant to be starting points for your own experimentation.
+
+Edit the program and run it again.
+
+When you click on Format (shortcut: Ctrl + Enter), the text in the editor is formatted using the gofmt tool. You can switch syntax highlighting on and off by clicking on the syntax button.
+
+When you're ready to move on, click the right arrow below or type the PageDown key.
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, 世界")
+}
