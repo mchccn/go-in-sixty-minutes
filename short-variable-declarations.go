ed6e68cf11bea7ef53a581823d39410d408eda5d
[Cursors] <65373746+cursorsdottsx@users.noreply.github.com>
2021-11-13 06:37:57 -0600

    Create short-variable-declarations.go

---
+++ short-variable-declarations.go
@@ -0,0 +1,17 @@
+/*
+Inside a function, the := short assignment statement can be used in place of a var declaration with implicit type.
+
+Outside a function, every statement begins with a keyword (var, func, and so on) and so the := construct is not available.
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	var i, j int = 1, 2
+	k := 3
+	c, python, java := true, false, "no!"
+
+	fmt.Println(i, j, k, c, python, java)
+}
